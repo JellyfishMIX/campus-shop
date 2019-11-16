0d72a6b01aa1906db7b1d6b64eda1442730eb338
JellyfishMIX <2273516339@qq.com>
2019-11-17 00:50:28 +0800

    :bug: Fixed a field name error in the ProductDao.xml :construction: Test of the ProductDao has been done

--- src/test/java/com/imooc/o2o/dao/ProductDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/ProductDaoTest.java
@@ -3,21 +3,29 @@ package com.imooc.o2o.dao;
 import com.imooc.o2o.BaseTest;
 import com.imooc.o2o.entity.Product;
 import com.imooc.o2o.entity.ProductCategory;
+import com.imooc.o2o.entity.ProductImg;
 import com.imooc.o2o.entity.Shop;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProductDaoTest extends BaseTest {
     @Autowired
     private ProductDao productDao;
+    @Autowired
+    ProductImgDao productImgDao;
 
     @Test
     @Ignore
-    public void testInsertProduct() {
+    public void testAInsertProduct() {
         Product product = new Product();
         product.setProductName("可乐");
         product.setProductDesc("测试可乐");
@@ -37,6 +45,59 @@ public class ProductDaoTest extends BaseTest {
         product.setShop(shop);
 
         int effectedNum = productDao.insertProduct(product);
+        Assert.assertEquals(1, effectedNum);
+    }
+
+    @Test
+    public void testBQueryProductByProductId() {
+        long productId = 1;
+        // 初始化两个商品详情图实例作为productId为1的商品下的详情图
+        // 批量插入到商品详情图表中
+        ProductImg productImg1 = new ProductImg();
+        productImg1.setImgAddr("图片1");
+        productImg1.setImgDesc("这是一条测试描述");
+        productImg1.setPriority(0);
+        productImg1.setCreateTime(new Date());
+        productImg1.setProductId(productId);
+
+        ProductImg productImg2 = new ProductImg();
+        productImg2.setImgAddr("图片2");
+        productImg2.setImgDesc("这是一条测试描述");
+        productImg2.setPriority(1);
+        productImg2.setCreateTime(new Date());
+        productImg2.setProductId(productId);
+
+        List<ProductImg> productImgList = new ArrayList<ProductImg>();
+        productImgList.add(productImg1);
+        productImgList.add(productImg2);
+        int effectedNum = productImgDao.batchInsertProductImg(productImgList);
+        Assert.assertEquals(2, effectedNum);
+
+        // 查询productId为1的商品信息并校验返回的详情图实例列表是否为2
+        Product product = productDao.queryProductByProductId(productId);
+        Assert.assertEquals(2, product.getProductImgList().size());
+
+        // 删除新增的这两个商品详情图实例
+        effectedNum = productImgDao.deleteProductImgByProductId(productId);
+        Assert.assertEquals(2, effectedNum);
+    }
+
+    @Test
+    public void testCUpdateProduct() {
+        Product product = new Product();
+        ProductCategory productCategory = new ProductCategory();
+        Shop shop = new Shop();
+
+        product.setProductId(1L);
+        product.setProductName("可乐");
+        shop.setShopId(2L);
+        productCategory.setProductCategoryId(1L);
+        product.setShop(shop);
+        product.setProductCategory(productCategory);
+
+        // 修改productId为1的商品的名称
+        // 以及商品类别并校验影响的行数是否为1
+        int effectedNum = productDao.updateProduct(product);
         Assert.assertEquals(1, effectedNum);
     }
 }
--- src/test/java/com/imooc/o2o/dao/ProductImgDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/ProductImgDaoTest.java
@@ -17,25 +17,35 @@ public class ProductImgDaoTest extends BaseTest {
 
     @Test
     @Ignore
-    public void testBatchInsertProductImg() {
+    public void testABatchInsertProductImg() {
+        long productId = 1;
         ProductImg productImg1 = new ProductImg();
         productImg1.setImgAddr("图片1");
         productImg1.setImgDesc("这是一条测试描述");
         productImg1.setPriority(0);
         productImg1.setCreateTime(new Date());
-        productImg1.setProductId(1L);
+        productImg1.setProductId(productId);
 
         ProductImg productImg2 = new ProductImg();
         productImg2.setImgAddr("图片2");
         productImg2.setImgDesc("这是一条测试描述");
         productImg2.setPriority(1);
         productImg2.setCreateTime(new Date());
-        productImg2.setProductId(1L);
+        productImg2.setProductId(productId);
 
         List<ProductImg> productImgList = new ArrayList<ProductImg>();
         productImgList.add(productImg1);
         productImgList.add(productImg2);
         int effectedNum = productImgDao.batchInsertProductImg(productImgList);
+        Assert.assertEquals(2, effectedNum);
+    }
+
+    @Test
+    @Ignore
+    public void testCDeleteProductImgByProductId() {
+        // 删除新增的两条商品详情图片记录
+        long productId = 1;
+        int effectedNum = productImgDao.deleteProductImgByProductId(productId);
         Assert.assertEquals(2, effectedNum);
     }
 }
