2f8800cb7ac23d314a71e2fafd94192d47e436eb
JellyfishMIX <2273516339@qq.com>
2019-12-04 00:44:23 +0800

    :sparkles: Add listShops

--- src/main/java/com/imooc/o2o/web/frontdesk/ShopListController.java
+++ src/main/java/com/imooc/o2o/web/frontdesk/ShopListController.java
@@ -1,6 +1,8 @@
 package com.imooc.o2o.web.frontdesk;
 
+import com.imooc.o2o.dto.ShopExecution;
 import com.imooc.o2o.entity.Area;
+import com.imooc.o2o.entity.Shop;
 import com.imooc.o2o.entity.ShopCategory;
 import com.imooc.o2o.service.AreaService;
 import com.imooc.o2o.service.ShopCategoryService;
@@ -72,5 +74,72 @@ public class ShopListController {
         }
 
         return modelMap;
+    }
+
+    @RequestMapping(value = "/listshops", method = RequestMethod.GET)
+    @ResponseBody
+    private Map<String, Object> listShops(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<>();
+        // 获取页码
+        int pageIndex = HttpServletRequestUtil.getInt(request, "pageIndex");
+        // 获取某一页需要显示的数据条数
+        int pageSize = HttpServletRequestUtil.getInt(request, "pageSize");
+        // 非空判断
+        if (pageIndex > -1 && pageSize > -1) {
+            // 试着获取一级类别Id
+            long parentId = HttpServletRequestUtil.getLong(request, "parentId");
+            // 试着获取特定二级类别Id
+            long shopCategoryId = HttpServletRequestUtil.getLong(request, "shopCategoryId");
+            // 试着获取区域Id
+            int areaId = HttpServletRequestUtil.getInt(request, "areaId");
+            // 试着获取模糊查询的名字
+            String shopName = HttpServletRequestUtil.getString(request, "shopName");
+            // 获取组合之后的查询条件
+            Shop shopCondition = compactShopCondition4Search(parentId, shopCategoryId, areaId, shopName);
+
+            // 根据查询条件和分页信息获取店铺列表，返回总数
+            ShopExecution shopExecution = shopService.getShopListAndCount(shopCondition, pageIndex, pageSize);
+
+            modelMap.put("success", true);
+            modelMap.put("shopList", shopExecution.getShopList());
+            modelMap.put("count", shopExecution.getCount());
+            return modelMap;
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "empty pageIndex or pageSize");
+            return modelMap;
+        }
+    }
+
+    private Shop compactShopCondition4Search(long parentId, long shopCategoryId, int areaId, String shopName) {
+        Shop shopCondition = new Shop();
+        // 查询某个一级shopCategory下面，所有二级shopCategory里面的店铺列表
+        if (parentId != -1L) {
+            ShopCategory parentCategory = new ShopCategory();
+            ShopCategory childCategory = new ShopCategory();
+            parentCategory.setShopCategoryId(parentId);
+            childCategory.setParent(parentCategory);
+            shopCondition.setShopCategory(childCategory);
+        }
+        // 查询某个二级shopCategory下面的店铺列表
+        if (shopCategoryId != 1L) {
+            ShopCategory shopCategory = new ShopCategory();
+            shopCategory.setShopCategoryId(shopCategoryId);
+            shopCondition.setShopCategory(shopCategory);
+        }
+        // 查询位于某个区域Id下的店铺列表
+        if (areaId != -1) {
+            Area area = new Area();
+            area.setAreaId(areaId);
+            shopCondition.setArea(area);
+        }
+        // 查询名字里包含shopName的店铺列表
+        if (shopName != null) {
+            shopCondition.setShopName(shopName);
+        }
+
+        // 前端展示的店铺都是审核成功后的合法店铺
+        shopCondition.setEnableStatus(1);
+        return shopCondition;
     }
 }
