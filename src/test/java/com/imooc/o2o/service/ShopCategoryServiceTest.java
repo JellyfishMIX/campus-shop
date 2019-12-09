aa731ff233f85a0793416104e79d4c50b297df00
JellyfishMIX <2273516339@qq.com>
2019-12-09 13:51:17 +0800

    :white_check_mark: Update ShopCategoryServiceTest

--- src/test/java/com/imooc/o2o/service/ShopCategoryServiceTest.java
+++ src/test/java/com/imooc/o2o/service/ShopCategoryServiceTest.java
@@ -1,11 +1,23 @@
 package com.imooc.o2o.service;
 
 import com.imooc.o2o.BaseTest;
+import com.imooc.o2o.entity.ShopCategory;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 
 public class ShopCategoryServiceTest extends BaseTest {
     @Autowired
     private ShopCategoryService shopCategoryService;
 
-
+    @Test
+    @Ignore
+    public void testGetShopCategoryList() {
+        List<ShopCategory> shopCategoryList = shopCategoryService.getShopCategoryList(null);
+        Assert.assertEquals(2, shopCategoryList.size());
+    }
 }
