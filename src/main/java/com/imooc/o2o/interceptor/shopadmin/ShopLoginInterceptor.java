e83929d802a8a13040a71c7d587b21d51d6bf9cd
JellyfishMIX <2273516339@qq.com>
2019-12-12 23:23:55 +0800

    :construction: Add interceptor layer

---
+++ src/main/java/com/imooc/o2o/interceptor/shopadmin/ShopLoginInterceptor.java
@@ -0,0 +1,7 @@
+package com.imooc.o2o.interceptor.shopadmin;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class ShopLoginInterceptor extends HandlerInterceptorAdapter {
+
+}
